6cd5906e3125da9b963537274814e8a889c58a2b
NateTheGreat <73970984+nateothegreato@users.noreply.github.com>
2021-06-20 21:29:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const server = require('./server')
+  const Discord = require('discord.js')
+  const client = new Discord.Client()
+  const server = require('./server')
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`)
-  
-  client.user.setPresence({ 
-    activity: {
-      name: 'Fortnite',
-      type: 'STREAMING',
-      url: 'https://twitch.tv/hsujddhdjagsdsksagdahkdgakjdgskaskjgd',
-    }
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+    
+    client.user.setPresence({ 
+      activity: {
+        name: 'Fortnite',
+        type: 'STREAMING',
+        url: 'https://twitch.tv/hsujddhdjagsdsksagdahkdgakjdgskaskjgd',
+      }
+    })
+    server(client)
   })
-  server(client)
-})
-client.login(process.env.TOKEN)
+  client.on('guildMemberAdd', member => {
+    let channel = client.channels.cache.get('854853818688339968')
+    if(!channel) return;
+    
+    channel.send(`Welcome to the server ${member}! Please read the rules and remember to verify ;)`)
+  })
+  client.on('guildMemberRemove', member => {
+    let channel = client.channels.cache.get('854853906970837062')
+    if(!channel) return;
+    
+    channel.send(`Goodbye **${member.user.tag}**, we hope you come back soon...`)
+  })
+
+  client.login(process.env.TOKEN)
